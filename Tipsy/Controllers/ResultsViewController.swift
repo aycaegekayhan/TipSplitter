44d391daa89e323c256733bb1f345ef6eb5112cb
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-22 22:23:08 -0500

    new view controller class created for results, iboutlets and ibaction added

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Ayça ege Kayhan on 11/22/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
